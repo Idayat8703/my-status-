b4d91d778101416e2c9392e6b52d903d8de3e293
Idayat <github email address>
2017-05-31 20:42:24 -0400

    setup schema

--- db/migrate/20170601002458_create_users.rb
+++ db/migrate/20170601002458_create_users.rb
@@ -3,7 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+      t.string :name
 
       t.timestamps
+    end
   end
 end
