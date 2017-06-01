9b865b700c7399dbe42ce812ca8476f926b63cfd
Idayat <github email address>
2017-05-31 21:03:25 -0400

    add namespace router for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      root "auth#ping"
+      resources :users, only: [:create, :show] do
+
+        resources :applications, only: [:index, :show, :create, :update, :destroy]
+
+      end
+      post'/auth', to: "auth#login"
+      post'/auth/refresh', to: "auth#refresh"
+
+    end
+  end
+
 end
