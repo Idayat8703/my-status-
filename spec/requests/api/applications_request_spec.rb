b0a73112e45748358b7eb258a81ce490a41cee88
Idayat <github email address>
2017-06-02 19:52:11 -0400

    testing api

--- spec/requests/api/applications_request_spec.rb
+++ spec/requests/api/applications_request_spec.rb
@@ -17,3 +17,22 @@ RSpec.describe "API::V1::Applications", type: :request do
         'Content-Type': 'application/json',
       }
     end
+
+    it "requires a token to make and edit an application" do
+      responses = []
+      response_bodies = []
+
+      post "/api/v1/users/#{@user.id}/applications", headers: @tokenless_headers
+      responses << response
+      response_bodies << JSON.parse(response.body)
+
+
+      patch "/api/v1/users/#{@user.id}/applications/#{@application.id}", headers: @tokenless_headers
+      responses << response
+      response_bodies << JSON.parse(response.body)
+
+      responses.each {|r| expect(r).to have_http_status(403)}
+      response_bodies.each {|body| expect(body["errors"]).to eq([{"message" => "You must include a JWT token"}])}
+    end
+
+  end
