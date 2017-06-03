3ee18d8893afe401dab578918c31cabb2423b66a
Idayat <github email address>
2017-06-02 20:11:42 -0400

    auth api routes

--- spec/requests/api/applications_request_spec.rb
+++ spec/requests/api/applications_request_spec.rb
@@ -37,71 +37,207 @@ RSpec.describe "API::V1::Applications", type: :request do
 
   end
 
-    describe "routes" do
+  describe "routes" do
 
-      before(:each) do
-        @user = create(:user)
-        3.times do
-          @user.applications.create(company: Faker::Company.name)
-        end
-        @application = @user.applications.first
-        @token = Auth.create_token(@user.id)
-        @token_headers = {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'Authorization': "Bearer: #{@token}"
-        }
+    before(:each) do
+      @user = create(:user)
+      3.times do
+        @user.applications.create(company: Faker::Company.name)
       end
-      describe "GET /api/v1/users/:id/applications" do
+      @application = @user.applications.first
+      @token = Auth.create_token(@user.id)
+      @token_headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer: #{@token}"
+      }
+    end
 
-        it "returns an array of applications belonging to a given user" do
+    describe "GET /api/v1/users/:id/applications" do
 
-          get "/api/v1/users/#{@user.id}/applications"
+      it "returns an array of applications belonging to a given user" do
+
+        get "/api/v1/users/#{@user.id}/applications"
+
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(body['applications']).to be_an(Array)
+        expect(body['applications'].size).to eq(3)
+      end
+
+      it "doesn't return applications belonging to other users" do
+        user2 = User.create(username: "user2", password: "password2")
+        application2 = user2.applications.create(company: "Wrong Company")
+
+        get "/api/v1/users/#{@user.id}/applications"
+        body = JSON.parse(response.body)
+        expect(body['applications'].last['user_id']).not_to eq(user2.id)
+      end
+    end
+
+    describe "GET /api/v1.users/:user_id/applications/:id" do
+
+      describe "on success" do
+
+        it "returns on application based on its id" do
+
+          get "/api/v1/users/#{@user.id}/applications/#{@application.id}"
 
           body = JSON.parse(response.body)
 
           expect(response.status).to eq(200)
-          expect(body['applications']).to be_an(Array)
-          expect(body['applications'].size).to eq(3)
+          expect(body['application']).not_to eq(nil)
+          expect(body['application']['id']).to eq(@application.id)
         end
 
-        it "doesn't return applications belonging to other users" do
-          user2 = User.create(username: "user2", password: "password2")
-          application2 = user2.applications.create(company: "Wrong Company")
-
-          get "/api/v1/users/#{@user.id}/applications"
-          body = JSON.parse(response.body)
-          expect(body['applications'].last['user_id']).not_to eq(user2.id)
-        end
       end
 
-      describe "GET /api/v1.users/:user_id/applications/:id" do
+      describe "on failure" do
+        it "returns a status of 404 with an error message" do
 
-        describe "on success" do
+          get "/api/v1/users/#{@user.id}/applications/fakeid"
 
-          it "returns on application based on its id" do
+          body = JSON.parse(response.body)
 
-            get "/api/v1/users/#{@user.id}/applications/#{@application.id}"
+          expect(response.status).to eq(404)
+          expect(body["errors"]).to eq({"message"=> "Page not found"})
 
-            body = JSON.parse(response.body)
-
-            expect(response.status).to eq(200)
-            expect(body['application']).not_to eq(nil)
-            expect(body['application']['id']).to eq(@application.id)
-          end
         end
-
-
-        describe "on failure" do
-          it "returns a status of 404 with an error message" do
-
-            get "/api/v1/users/#{@user.id}/applications/fakeid"
-
-            body = JSON.parse(response.body)
-
-            expect(response.status).to eq(404)
-            expect(body["errors"]).to eq({"message"=> "Page not found"})
-
-          end
-        end
+      end
     end
+
+    describe "POST /api/v1/users/:id/applications" do
+      params = {
+        application: {
+          company: 'Google',
+          contact_name: 'Larry Page',
+          contact_title: 'CEO',
+          date: '05/15/17',
+          action: "Meeting",
+          first_contact: false,
+          job_title: 'Lead Developer',
+          job_url: 'google.com',
+          notes: 'Test data',
+          complete: false,
+          next_step: "Get Job",
+          status: nil
+        }
+      }
+
+      before(:each) do
+        post "/api/v1/users/#{@user.id}/applications",
+          params: params.to_json,
+          headers: @token_headers
+
+        @body = JSON.parse(response.body)
+      end
+
+      it "creates a new instance of an Application" do
+
+        application = Application.last
+        expect(application.company).to eq(params[:application][:company])
+        expect(application.contact_name).to eq(params[:application][:contact_name])
+        expect(application.contact_title).to eq(params[:application][:contact_title])
+        expect(application.date).to eq(params[:application][:date])
+        expect(application.action).to eq(params[:application][:action])
+        expect(application.first_contact).to eq(params[:application][:first_contact])
+        expect(application.job_title).to eq(params[:application][:job_title])
+        expect(application.job_url).to eq(params[:application][:job_url])
+        expect(application.notes).to eq(params[:application][:notes])
+        expect(application.complete).to eq(params[:application][:complete])
+        expect(application.next_step).to eq(params[:application][:next_step])
+        expect(application.status).to eq(params[:application][:status])
+      end
+
+      it "returns the new Application" do
+        application = Application.last
+
+        expect(@body['application']['id']).to eq(application.id)
+        expect(@body['application']['company']).to eq(application.company)
+      end
+    end
+
+    describe "PATCH /api/v1/users/:id/applications" do
+      params = {
+        application: {
+          company: 'Google',
+          contact_name: 'Larry Page',
+          contact_title: 'CEO',
+          date: '05/15/17',
+          action: "Meeting",
+          first_contact: false,
+          job_title: 'Lead Developer',
+          job_url: 'google.com',
+          notes: 'Test data',
+          complete: false,
+          next_step: "Get Job",
+          status: nil
+        }
+      }
+
+      before(:each) do
+        patch "/api/v1/users/#{@user.id}/applications/#{@user.applications.last.id}",
+          params: params.to_json,
+          headers: @token_headers
+
+        @body = JSON.parse(response.body)
+      end
+
+      it "updates the application" do
+        application = Application.last
+        expect(application.company).to eq(params[:application][:company])
+        expect(application.contact_name).to eq(params[:application][:contact_name])
+        expect(application.contact_title).to eq(params[:application][:contact_title])
+        expect(application.date).to eq(params[:application][:date])
+        expect(application.action).to eq(params[:application][:action])
+        expect(application.first_contact).to eq(params[:application][:first_contact])
+        expect(application.job_title).to eq(params[:application][:job_title])
+        expect(application.job_url).to eq(params[:application][:job_url])
+        expect(application.notes).to eq(params[:application][:notes])
+        expect(application.complete).to eq(params[:application][:complete])
+        expect(application.next_step).to eq(params[:application][:next_step])
+        expect(application.status).to eq(params[:application][:status])
+      end
+
+      it "returns the updated Application" do
+        application = Application.last
+
+        expect(@body['application']['id']).to eq(application.id)
+        expect(@body['application']['company']).to eq(application.company)
+      end
+    end
+
+    describe "DELETE /api/v1.users/:user_id/applications/:id" do
+
+      describe "on success" do
+
+        it "deletes the application and returns a status of 204" do
+          delete "/api/v1/users/#{@user.id}/applications/#{@application.id}",
+            headers: @token_headers
+
+
+          expect(Application.find_by(id: @application.id)).to eq(nil)
+          expect(response.status).to eq(204)
+
+        end
+
+      end
+
+      describe "on failure" do
+        it "returns a status of 404 with an error message" do
+
+          delete "/api/v1/users/#{@user.id}/applications/fakeid",
+            headers: @token_headers
+
+
+          body = JSON.parse(response.body)
+          expect(response.status).to eq(404)
+          expect(body["errors"]).to eq({"message"=> "Page not found"})
+
+        end
+      end
+    end
+  end
+
+end
